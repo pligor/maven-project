62f71095f2726dc85fd4b6ff35050509a3a32774
Georgios Pligoropoulos <george@pligor.com>
2018-04-11 19:20:53 +0100

    added a fully automated jenkins pipeline

---
+++ jenkins_pipeline.groovy
@@ -0,0 +1,43 @@
+pipeline {
+    agent any
+
+    parameters {
+        string(name: 'tomcat_dev', defaultValue: '34.244.212.162', description: 'staging server')
+        string(name: 'tomcat_prod', defaultValue: '34.244.53.38', description: 'production server')
+    }
+
+    triggers {
+        pollSCM('* * * * *') //every minute for demo purposes (cron job)
+    }
+
+    stages {
+        stage('build') {
+            steps {
+                sh 'mvn clean package'
+            }
+
+            post {
+                success {
+                    echo 'Now Archiving...'
+                    archiveArtifacts artifacts: '**/target/*.war'
+                }
+            }
+        }
+
+        stage('deployments') {
+            parallel {
+                stage('deploy to staging') {
+                    steps {
+                        sh "scp -i /home/student/.ssh/tomcat_dev.pem **/target/*.war ec2-user@${params.tomcat_dev}:/var/lib/tomcat7/webapps"
+                    }
+                }
+
+                stage('deploy to production') {
+                    steps {
+                        sh "scp -i /home/student/.ssh/tomcat_prod.pem **/target/*.war ec2-user@${params.tomcat_prod}:/var/lib/tomcat7/webapps"
+                    }
+                }
+            }
+        }
+    }
+}
--- webapp/src/main/webapp/index.jsp
+++ webapp/src/main/webapp/index.jsp
@@ -1,3 +1,5 @@
 Say hello to my little friend
 <br/>
 Asta la vista baby
+<br/>
+Bite me blondie
